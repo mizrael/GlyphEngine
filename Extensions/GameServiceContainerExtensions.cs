618e8080b1c932c34a975a5ec5ca59ef457138dd
mizrael <davide.guida@razorfishhealthware.com>
2014-01-17 12:45:02 +0100

    minor refactorings

---
+++ Extensions/GameServiceContainerExtensions.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.Xna.Framework;
+
+namespace GlyphEngine.Extensions
+{
+    public static class GameServiceContainerExtensions
+    {
+        public static T GetService<T>(this GameServiceContainer container) where T : class
+        {
+            return container.GetService(typeof(T)) as T;
+        }
+    }
+}
---
+++ Extensions/MathExtensions.cs
@@ -0,0 +1,10 @@
+﻿
+namespace GlyphEngine.Extensions
+{
+    public static class MathExtensions
+    {
+        public static bool IsEven(this int number) {
+            return 0 == (number & 1);
+        }
+    }
+}
